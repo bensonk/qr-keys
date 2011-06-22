c5f7505a39504cf4aa12be98206d9749a3aa93a3
Benson Kalahar <bensonk42@gmail.com>
2011-06-21 21:55:19 -0400

    Now generates HTML doc, fleshed out readme.

--- qr-keys.sh
+++ qr-keys.sh
@@ -6,3 +6,18 @@ cat $PUB | qrencode -o $PUB".png"
 
 convert -negate -colorize 55,0,0 -negate $PRIV.png $PRIV.png
 convert -negate -colorize 0,0,55 -negate $PUB.png $PUB.png
+
+cat >$PRIV.html <<END
+<html>
+  <head><title>$PRIV | $PUB</title></head>
+  <body>
+   <div id="public"><img src="$PUB.png"></div>
+   <div id="private"><img src="$PRIV.png"></div>
+  </body>
+</html>
+END
+open $PRIV.html
+echo "Your keytags have been created:"
+echo "	$PRIV.png"
+echo "	$PUB.png"
+echo "	$PRIV.html"
